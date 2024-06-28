d49158e926f9a6784c8d1fd8d0bd555d263d1a13
manishbisht12 <154014370+manishbisht12@users.noreply.github.com>
2024-06-28 17:54:48 +0530

    Add files via upload

---
+++ p6.c
@@ -0,0 +1,21 @@
+//Reverse string
+
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str[100]= "Hello world";
+    printf("Original String: %s\n", str);
+
+    int len= strlen(str);
+    for(int i=0, j= len - 1; i < j; i++ ,j--) {
+        char c = str[i];
+        str[i] = str[j];
+        str[j]= c;
+
+    }
+
+    printf("Reversed String: %s", str);
+    return 0;
+}
